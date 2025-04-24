69f83bb348b82415909f99b1d5846da19b3c2f0e
CBananaC <liukaiwangkelvin@gmail.com>
2025-04-25 01:19:07 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public', 'introduction.html'));
+    res.sendFile(path.join(__dirname, '..', 'public', 'cover.html'));
 });
 
 // ✅ 讀取與快取 JSON 資料
